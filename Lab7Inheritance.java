1567f5b2e40c7e543181f91030ffc33683120214
Laika May <66331364+ChinamaykzaLaika@users.noreply.github.com>
2022-04-13 10:47:55 -0400

    Update Lab7Inheritance.java

--- Lab7Inheritance.java
+++ Lab7Inheritance.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package pkg1213module8;
+package lab7inheritance;
 import java.util.*;
 
 /**
